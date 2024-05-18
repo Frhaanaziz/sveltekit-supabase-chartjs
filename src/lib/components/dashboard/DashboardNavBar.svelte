b3661b08c643d4090aa27fd0bfeda88c1836593b
Farhan <farhan@aththariq.com>
2024-05-18 22:25:42 +0700

    chore: Update theme configuration and toggle button styling

--- src/lib/components/dashboard/DashboardNavBar.svelte
+++ src/lib/components/dashboard/DashboardNavBar.svelte
@@ -27,9 +27,9 @@
 	<div class="flex-none mx-3">
 		<label class="btn btn-ghost btn-circle swap swap-rotate">
 			<!-- this hidden checkbox controls the state -->
-			<input data-toggle-theme="business, light" data-act-class="ACTIVECLASS" type="checkbox" />
+			<input data-toggle-theme="dark,light" data-act-class="ACTIVECLASS" type="checkbox" />
 			<MoonIcon class="swap-on fill-current" />
-			<SunIcon class="swap-off fill-curren" />
+			<SunIcon class="swap-off fill-current" />
 		</label>
 	</div>
 
