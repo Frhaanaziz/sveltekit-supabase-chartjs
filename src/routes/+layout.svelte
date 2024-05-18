fe0387321241196f69a9f84595e83364f834b528
Farhan <farhan@aththariq.com>
2024-05-18 10:36:43 +0700

    refactor: Remove Google Analytics script from layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,16 +6,6 @@
 <svelte:head>
 	<title>{$page.url.hostname}</title>
 	<link rel="icon" type="image/x-icon" href={`/${$page.url.hostname}/favicon.ico`} />
-
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-J1TGFG0V29"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-		gtag('config', 'G-J1TGFG0V29');
-	</script>
 </svelte:head>
 
 <slot />
