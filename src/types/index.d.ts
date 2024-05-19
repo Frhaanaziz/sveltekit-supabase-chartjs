96edd841436c06a0d7096a139b785cb6e84b0505
Farhan <farhan@aththariq.com>
2024-05-19 21:30:20 +0700

    refactor: Update index.d.ts to include Navigation type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,14 @@
+import type * as Icons from 'svelte-feather-icons';
 import { Tables } from './supabase';
 
 export type Organization = Tables<'orgs'>;
 export type Profiles = Tables<'profiles'>;
+export type Navigation = {
+	name: string;
+	items: {
+		title: string;
+		href: string;
+		// icon: keyof typeof Icons;
+		Icon: (typeof Icons)[keyof typeof Icons];
+	}[];
+};
