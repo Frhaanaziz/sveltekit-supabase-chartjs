4dae20b8b78ad6c85cc95c4ce80ebdbc1469fa72
Farhan <farhan@aththariq.com>
2024-05-19 21:32:36 +0700

    refactor: Update dashboard layout component to use DashboardLayout wrapper

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import { navigating } from '$app/stores';
-	import DashboardMenu from '$lib/components/dashboard/DashboardMenu.svelte';
-	import DashboardNavBar from '$lib/components/dashboard/DashboardNavBar.svelte';
-	import DemoUserNotice from '$lib/components/DemoMode/DemoUserNotice.svelte';
+	import DashboardLayout from '$lib/components/dashboard/layout/DashboardLayout.svelte';
 	import { onMount } from 'svelte';
-	import { Jumper } from 'svelte-loading-spinners';
 	import { themeChange } from 'theme-change';
 
 	// NOTE: the element that is using one of the theme attributes must be in the DOM on mount
@@ -14,23 +10,6 @@
 	});
 </script>
 
-<section id="body" class="flex flex-row h-screen">
-	<DashboardMenu />
-
-	<div class="flex flex-col w-full">
-		<DashboardNavBar />
-		<!-- CONTENT -->
-		<div class="w-full h-full p-5 overflow-auto">
-			{#if $navigating}
-				<div class="flex h-full items-center justify-center">
-					<Jumper size="120" unit="px" duration="1s" color="#FFFFFF" />
-				</div>
-			{:else}
-				<slot />
-			{/if}
-		</div>
-		<div class="w-full px-5 overflow-none">
-			<DemoUserNotice />
-		</div>
-	</div>
-</section>
+<DashboardLayout>
+	<slot />
+</DashboardLayout>
