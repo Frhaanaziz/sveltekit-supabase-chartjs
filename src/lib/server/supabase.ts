ae3efddf595c79e5f66588a62df44fdf95ca53dd
Farhan <farhan@aththariq.com>
2024-05-20 21:08:48 +0700

    refactor: Update supabase.js to use TypeScript

--- src/lib/server/supabase.ts
+++ src/lib/server/supabase.ts
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { PRIVATE_SUPABASE_SERVICE_KEY } from '$env/static/private';
+import type { Database } from '$types/supabase';
 
-export const supabaseAdminClient = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_SERVICE_KEY);
+export const supabaseAdminClient = createClient<Database>(
+	PUBLIC_SUPABASE_URL,
+	PRIVATE_SUPABASE_SERVICE_KEY
+);
