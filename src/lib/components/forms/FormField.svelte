c0812061eb79dd383603665648c7629144cf3a30
Farhan <farhan@aththariq.com>
2024-05-20 21:09:37 +0700

    feat: Add FormField component for reusable form fields

---
+++ src/lib/components/forms/FormField.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let error: string[] | undefined;
+	export let label: string;
+</script>
+
+<label class="form-control">
+	<div class="label">
+		<span class="label-text">{label}</span>
+	</div>
+
+	<slot />
+
+	{#if error}
+		<div class="label">
+			<span class="label-text-alt text-error">{error}</span>
+		</div>
+	{/if}
+</label>
