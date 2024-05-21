90794eb7c3734e2e7cbe3a719965d4a2a8c5176b
Farhan <farhan@aththariq.com>
2024-05-21 21:13:53 +0700

    refactor: Add RelativeDate component to display relative time in dashboard

---
+++ src/lib/components/dashboard/RelativeDate.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { formatDateWithTime, getRelativeTimeString } from '$lib/utils';
+
+	export let row: any;
+	export let column: any;
+
+	$: value = row[column];
+</script>
+
+<div class="tooltip" data-tip={formatDateWithTime(value)}>
+	<span class="hover:underline">{getRelativeTimeString(value)}</span>
+</div>
