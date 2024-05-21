b3233d97e39bbb24b655503eebf3082e9d138101
Farhan <farhan@aththariq.com>
2024-05-21 15:52:40 +0700

    feat: Add Pagination component for navigating through pages

---
+++ src/lib/components/dashboard/Pagination.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let currentPage: number,
+		isFirstPage: boolean,
+		isLastPage: boolean,
+		nextPage: number,
+		previousPage: number,
+		totalPages: number,
+		pathname: string;
+
+	function createQueryString(page: string) {
+		const params = new URLSearchParams();
+		params.append('page', page);
+		return params.toString();
+	}
+</script>
+
+<div class="flex items-center justify-between">
+	<p class="font-semibold">Page {currentPage} of {totalPages}</p>
+
+	<div class="join">
+		<a
+			class="join-item btn btn-sm btn-square"
+			class:btn-disabled={isFirstPage}
+			href={`${pathname}?${createQueryString(previousPage.toString())}`}>«</a
+		>
+		<button class="join-item btn btn-sm no-animation">Page {currentPage}</button>
+		<a
+			class="join-item btn btn-sm btn-square"
+			class:btn-disabled={isLastPage}
+			href={`${pathname}?${createQueryString(nextPage.toString())}`}>»</a
+		>
+	</div>
+</div>
