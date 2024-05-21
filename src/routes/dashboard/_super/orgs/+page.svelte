10815384e242acd36c408e7ae52203ff9f547d11
Farhan <farhan@aththariq.com>
2024-05-21 21:18:51 +0700

    refactor: Update orgs page with table, pagination, and create organization form

--- src/routes/dashboard/_super/orgs/+page.svelte
+++ src/routes/dashboard/_super/orgs/+page.svelte
@@ -1,22 +1,33 @@
 <script lang="ts">
-	import { applyAction, deserialize, enhance } from '$app/forms';
+	import { applyAction, deserialize } from '$app/forms';
 	import { invalidateAll } from '$app/navigation';
 	import { page } from '$app/stores';
 	import DashboardPage from '$lib/components/dashboard/DashboardPage.svelte';
-	import OrgsTable from '$lib/components/dashboard/OrgsTable.svelte';
 	import type { Organization } from '$types';
 	import type { ActionResult } from '@sveltejs/kit';
 	import toast from 'svelte-french-toast';
 	import type { ActionData, PageData } from './$types';
+	import SvelteTable, { type TableColumns } from 'svelte-table';
+	import TableActions from '$lib/components/dashboard/TableActions.svelte';
+	import { PlusIcon } from 'svelte-feather-icons';
+	import Pagination from '$lib/components/dashboard/Pagination.svelte';
+	import CreateOrgForm from '$lib/components/forms/CreateOrgForm.svelte';
+	import { superForm as superFormApi } from 'sveltekit-superforms/client';
+	import { zod } from 'sveltekit-superforms/adapters';
+	import { createOrganizationSchema } from '$lib/validators/organization';
+	import OrgTableAction from '$lib/components/dashboard/OrgTableAction.svelte';
 
 	export let data: PageData;
 	export let form: ActionData;
 
-	const orgs = data.orgs;
-	if (!orgs) throw new Error('orgs not found in data');
-
+	$: orgsData = data.orgsData;
+	$: orgs = orgsData?.content;
 	const pathname = $page.url.pathname;
-	let view = 'home';
+	let isModalOpen = false;
+
+	const superForm = superFormApi(data.form!, {
+		validators: zod(createOrganizationSchema)
+	});
 
 	// https://kit.svelte.dev/docs/form-actions#progressive-enhancement-custom-event-listener
 	const deleteAction = async (id: Organization['id']) => {
@@ -47,59 +58,72 @@
 	};
 
 	$: if (form?.error) toast.error('Failed to add organization, please try again');
+
+	const columns = [
+		{
+			key: 'name',
+			title: 'Name',
+			value: (v: any) => v.name,
+			sortable: true
+		},
+		{
+			key: 'created_by',
+			title: 'Created by',
+			value: (v: any) => v.created_by.name,
+			sortable: true
+		},
+		{
+			key: 'created_at',
+			title: 'Created at',
+			value: (v: any) => v.created_at,
+			sortable: true
+		},
+		{
+			key: 'delete-action',
+			title: 'Actions',
+			renderComponent: {
+				component: OrgTableAction
+			}
+		}
+	] satisfies TableColumns<Organization>;
 </script>
 
-<!-- https://svelte.dev/repl/b17c13d4f1bb40799ccf09e0841ddd90?version=3.55.0 -->
-{#if view == 'home'}
+{#if orgsData && orgs && orgs.length > 0}
 	<DashboardPage {pathname}>
 		<span slot="title">Organizations</span>
-		<!-- <button
-				class="btn btn-primary gap-2"
-				on:click={() => {
-					view = 'add';
-				}}
-			>
-				<PlusIcon class="h-4 w-4" />
-				add org
-			</button> -->
-		<span slot="content" class="w-full">
-			<OrgsTable orgs={data.orgs} {onAction} />
-		</span>
-	</DashboardPage>
-{:else if view == 'add'}
-	<DashboardPage {pathname}>
-		<span slot="title">Add organization</span>
-		<!-- <button
-				on:click={() => {
-					view = 'home';
-				}}
-				class="btn btn-primary"
-			>
-				<XIcon class="mr-2 h-4 w-4" />
-				CANCEL
-			</button> -->
-		<span slot="content" class="w-full">
-			<form id="user" method="POST" action="?/create" enctype="multipart/form-data" use:enhance>
-				<div class="form-control gap-y-3">
-					<div class="flex flex-row">
-						<label class="input-group w-full">
-							<span class="text-xl w-1/4 min-w-min bg-primary">Name</span>
-							<input
-								autocomplete="name"
-								id="name"
-								name="name"
-								class="w-full input input-bordered"
-								type="text"
-								placeholder="Organization name"
-								required
-							/>
-						</label>
-					</div>
+		<div slot="content" class="bg-base-100 p-5 rounded">
+			<div class="flex items-center mb-5">
+				<p class="text-lg font-semibold">
+					Total organizations <span class="text-primary font-bold">({orgsData.totalRow})</span>
+				</p>
+
+				<div class="tooltip ml-auto" data-tip="Create user">
+					<label for="add_org_modal" class="btn btn-outline btn-square btn-sm">
+						<PlusIcon class="w-4 h-4" />
+					</label>
 				</div>
-				<div class="form-control mt-6">
-					<button class="btn btn-primary">ADD ORGANIZATION</button>
-				</div>
-			</form>
-		</span>
+			</div>
+			<SvelteTable
+				{columns}
+				rows={orgs}
+				classNameTable={'table divide-y'}
+				classNameTbody={'divide-y'}
+			/>
+			<div class="w-full mt-7">
+				<Pagination {...orgsData} {pathname} />
+			</div>
+		</div>
 	</DashboardPage>
 {/if}
+
+<input type="checkbox" id="add_org_modal" class="modal-toggle" bind:checked={isModalOpen} />
+<div class="modal" role="dialog" class:modal-open={isModalOpen}>
+	<div class="modal-box max-w-lg">
+		<button
+			class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+			on:click={() => (isModalOpen = false)}>✕</button
+		>
+
+		<CreateOrgForm {superForm} formAction={form} on:closeModal={() => (isModalOpen = false)} />
+	</div>
+</div>
