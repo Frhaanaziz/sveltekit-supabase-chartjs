d9c5b9202035e20be9720fc2ead6a0b77bfb6bfd
Farhan <farhan@aththariq.com>
2024-05-21 15:54:32 +0700

    refactor: Update DashboardHeader to display user avatar and name dynamically

--- src/lib/components/dashboard/layout/DashboardHeader.svelte
+++ src/lib/components/dashboard/layout/DashboardHeader.svelte
@@ -1,9 +1,14 @@
-<script>
+<script lang="ts">
+	import type { Session, User } from '@supabase/supabase-js';
+	import { getContext } from 'svelte';
 	import { BellIcon, ChevronDownIcon, LogOutIcon, MenuIcon, UserIcon } from 'svelte-feather-icons';
+
+	const session = getContext('session') as Session;
+	const user = session.user;
 </script>
 
 <div
-	class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 px-4 border-b sm:gap-x-6 sm:px-6 lg:px-8"
+	class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 px-4 border-b sm:gap-x-6 sm:px-6 lg:px-8 bg-base-100 shadow"
 >
 	<label for="my-drawer-2" class="-m-2.5 p-2.5 lg:hidden">
 		<span class="sr-only">Open sidebar</span>
@@ -26,11 +31,13 @@
 
 					<img
 						class="h-8 w-8 rounded-full"
-						src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-						alt=""
+						src={user?.app_metadata?.avatar ?? '/avatar-fallback.png'}
+						alt={user?.app_metadata?.name ?? 'YOUR NAME'}
 					/>
 					<span class="hidden lg:flex lg:items-center gap-2">
-						<span class="text-sm font-semibold leading-6">Tom Cook</span>
+						<span class="text-sm font-semibold leading-6"
+							>{user?.app_metadata?.name ?? 'YOUR NAME'}</span
+						>
 						<ChevronDownIcon class="h-5 w-5" />
 					</span>
 				</div>
