8a30f0dbe4270fa6d2275d23625960f8983378c6
Farhan <farhan@aththariq.com>
2024-05-21 17:11:31 +0700

    refactor: Update user update page to use SuperForms for profile picture upload

--- src/routes/dashboard/_admin/users/[id]/update/+page.svelte
+++ src/routes/dashboard/_admin/users/[id]/update/+page.svelte
@@ -6,13 +6,15 @@
 	import { zod } from 'sveltekit-superforms/adapters';
 	import { updateUserSchema } from '$lib/validators/user';
 	import { page } from '$app/stores';
+	import type { Profile } from '$types';
+	import { TrashIcon, UploadIcon } from 'svelte-feather-icons';
 
 	export let data: PageData;
 	export let form: ActionData;
+	export let profile: Profile;
 
 	const orgs = data.orgs;
-	const session = data.session;
-	if (!orgs || !session) throw new Error('Missing data');
+	if (!orgs) throw new Error('Missing data');
 	const superForm = superFormApi(data.form, {
 		validators: zod(updateUserSchema)
 	});
@@ -24,6 +26,30 @@
 	<span slot="title">Update user</span>
 
 	<div slot="content" class="bg-base-100 p-5 rounded">
+		<div class="flex flex-col sm:flex-row items-center gap-5">
+			<img
+				src={profile?.avatar ?? '/avatar-fallback.png'}
+				alt={profile?.name}
+				width="100"
+				height="100"
+				class="rounded"
+			/>
+			<div class="flex gap-3 flex-col">
+				<p class="text-lg font-semibold">Profile Picture</p>
+				<div class="flex items-center gap-2">
+					<button class="btn btn-xs btn-primary gap-2 rounded">
+						<UploadIcon class="w-3.5 h-3.5" />
+						Upload Image</button
+					>
+					<button class="btn btn-xs gap-2 rounded">
+						<TrashIcon class="w-3.5 h-3.5" />
+						Remove</button
+					>
+				</div>
+				<p class="text-xs">We support PNG, JPEG, JPG under 1MB</p>
+			</div>
+		</div>
+
 		<UpdateUserForm formAction={form} {superForm} {orgs} />
 	</div>
 </DashboardPage>
