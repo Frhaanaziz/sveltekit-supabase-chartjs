e900527a3db57c3da92f72ed8f090c84e4682dba
Farhan <farhan@aththariq.com>
2024-05-22 20:56:11 +0700

    refactor: Update page title in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { Toaster } from 'svelte-french-toast';
 	import '../app.postcss';
 </script>
 
 <svelte:head>
-	<title>{$page.url.hostname}</title>
-	<link rel="icon" type="image/x-icon" href={`/${$page.url.hostname}/favicon.ico`} />
+	<title>Pipeline</title>
 </svelte:head>
 
-<slot />
 <Toaster position={'top-center'} />
+<slot />
