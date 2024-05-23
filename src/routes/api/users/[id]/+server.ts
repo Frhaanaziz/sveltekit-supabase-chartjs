30cdb245fdf17a62ebae32b9153ce435bda2b961
Farhan <farhan@aththariq.com>
2024-05-23 23:44:03 +0700

    refactor: Delete user profile when deleting user

--- src/routes/api/users/[id]/+server.ts
+++ src/routes/api/users/[id]/+server.ts
@@ -1,19 +1,13 @@
 import { supabaseAdminClient } from '$lib/server/supabase.js';
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 
 export async function DELETE({ params }) {
 	const { id } = params;
 
-	const profileRes = await supabaseAdminClient.from('profiles').delete().eq('id', id);
-	if (profileRes.error) {
-		console.error('Error deleting profile', profileRes.error);
-		return json({ error: 'Error deleting profile' }, { status: 500 });
-	}
-
 	const res = await supabaseAdminClient.auth.admin.deleteUser(id);
 	if (res.error) {
 		console.error('Error deleting user', res.error);
-		return json({ error: 'Error deleting user' }, { status: 500 });
+		error(500, 'Error deleting user');
 	}
 
 	return json({ success: true }, { status: 200 });
