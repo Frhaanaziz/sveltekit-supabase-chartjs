0f9f6986fe11cc1b9a5e1bfbb45865426f6d2a88
Farhan <farhan@aththariq.com>
2024-05-24 17:25:57 +0700

    refactor: Update redirect URL in dashboard layout server code

--- src/routes/dashboard/+layout.server.ts
+++ src/routes/dashboard/+layout.server.ts
@@ -8,7 +8,7 @@ import type { LayoutServerLoad } from './$types';
  */
 export const load: LayoutServerLoad = async ({ locals: { getUser } }) => {
 	const user = await getUser();
-	if (!user) redirect(308, '/auth');
+	if (!user) redirect(308, '/auth/login');
 
 	return { user };
 };
