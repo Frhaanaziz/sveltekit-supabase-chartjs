ca2fceb1aedb7438585fba7203efe4a9fbb9410d
Farhan <farhan@aththariq.com>
2024-05-24 17:24:49 +0700

    refactor: Update page layout in auth route and add hero image

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,11 @@
+<section class="grid grid-cols-1 md:grid-cols-12 min-h-screen">
+	<div
+		class="hidden md:col-span-7 lg:col-span-8 xl:col-span-9 bg-[#FFE9D1] md:flex md:items-center md:justify-center"
+	>
+		<img src="/auth-hero.png" alt="" />
+	</div>
+	<div class="p-8 lg:p-16 md:col-span-5 lg:col-span-4 xl:col-span-3">
+		<p class="text-primary/80 text-2xl font-semibold">Pipeline</p>
+		<slot />
+	</div>
+</section>
